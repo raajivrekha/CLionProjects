33ef9a40895bbadc10feefe79bbb151565b330fc
Raajiv Rekha <raajivrekha@gmail.com>
2018-02-14 13:57:35 -0500

    Some Beginner Examples

---
+++ Outputting Text/Outputting Text.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Raajiv Rekha on 2/14/18.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Starting a program..." << flush;
+    cout << "This is some text." << endl;
+    cout << "This is some more text." << endl;
+    cout << "Banana. " << "Apple. " << "Orange. " << endl;
+
+    return 0;
+
+}
---
+++ Strings/Strings.cpp
@@ -0,0 +1,18 @@
+// Name        : Strings.cpp
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string text1 = "hello ";
+    string text2 = "test";
+    string text3 = text1 + text2;
+
+
+    //cout << text1 << text2 << endl;
+    cout << text3 << endl;
+
+    return 0;
+}
